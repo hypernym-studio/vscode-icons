b9b686cdb71b8b72cebe72671d8ff2e2725e4816
Ivo Dolenc <ivodolenc@gmail.com>
2024-09-19 18:23:10 +0200

    refactor: update imports

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,9 @@
-import { writeFile } from 'node:fs/promises'
+import { write } from '@hypernym/utils/fs'
 import { theme } from './theme/index.js'
 import { paths } from './utils/index.js'
 
 async function generateExtension() {
-  return await writeFile(paths.extensionFile, theme)
+  return await write(paths.extensionFile, theme)
 }
 
 generateExtension()
