2e2af9a8f4b46377ab19461eaec6d7e49fdab487
Ivo Dolenc <ivodolenc@gmail.com>
2024-10-04 23:14:54 +0200

    chore: update theme build scripts

--- scripts/build-theme.ts
+++ scripts/build-theme.ts
@@ -1,9 +1,11 @@
 import { write } from '@hypernym/utils/fs'
 import { paths } from '@/utils'
 import { createIconTheme } from '@/theme'
+import { defaultConfig } from '@/extension/default-config'
 
 async function buildTheme(): Promise<void> {
-  const theme = createIconTheme()
+  const config = defaultConfig()
+  const theme = createIconTheme(config)
   const json = JSON.stringify(theme, null, 2)
   await write(paths.file.theme, json)
 }
