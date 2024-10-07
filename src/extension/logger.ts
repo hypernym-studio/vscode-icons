1a58556a7445f1ac4b3e1295a78e45d177f13f62
Ivo Dolenc <ivodolenc@gmail.com>
2024-10-07 13:51:19 +0200

    feat: add extension messages notifications

--- src/extension/logger.ts
+++
@@ -1,6 +0,0 @@
-import { extensionTitle } from '@/utils'
-
-export const logger = {
-  info: (message: string) => `${extensionTitle} ${message}`,
-  error: (message: string) => `${extensionTitle} ${message}`,
-}
